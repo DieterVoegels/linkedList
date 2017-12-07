250e05dbdc04ef1365ea8f8ae3b5714325d9fed0
U-BSD\333369 <333369@SHS-10D4C15EA.beaverton.k12.or.us>
2017-12-07 12:23:45 -0800

    I hate this

---
+++ student.cpp
@@ -0,0 +1,11 @@
+//test student
+#include <iostream>
+#include "student.h"
+
+student::student(){
+  id = 1;
+}
+
+int student::getValue(){
+  return id;
+}
---
+++ student.h
@@ -0,0 +1,12 @@
+//test student h file
+#include <iostream>
+
+using namespace std;
+
+class student{
+ public:
+  student();
+  int getValue();
+ private:
+  int id;
+};
